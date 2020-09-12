8ddd1b421343ed9d78a2c184355a7e287d93da94
Christian Quesada <cd1414@gmail.com>
2020-09-12 15:03:18 -0600

    Update calculator.js

--- tests/calculator.js
+++ tests/calculator.js
@@ -1,46 +1,7 @@
 let homepage = require('../pages/homepage');
 
-// first version
-// describe('demo calculator tests', function () {
+// POM
 
-//     beforeEach(() => {
-//         // open browser
-//         browser.get('https://juliemr.github.io/protractor-demo/');
-//     })
-
-
-//     it('addition test', function () {
-//         //enter data
-//         element(by.model('first')).sendKeys(3);
-//         element(by.model('second')).sendKeys(2);
-
-//         // click on button
-//         element(by.css('[ng-click="doAddition()"]')).click();
-
-//         // verify result
-//         let result = element(by.cssContainingText('.ng-binding','5'));
-//         expect(result.getText()).toEqual('5');
-//     });
-
-//     it('substration test', function () {
-//         //enter data        
-//         element(by.model('first')).sendKeys(3);
-//         element(by.model('second')).sendKeys(2);
-//         element(by.model('operator')).sendKeys('-');
-
-//         // click on button
-//         element(by.css('[ng-click="doAddition()"]')).click();
-
-//         // get result
-//         let result = element(by.cssContainingText('.ng-binding', '1'));
-
-//         // verified the result
-//         expect(result.getText()).toEqual('1');
-//     });
-
-// })
-
-// POM version
 describe('demo calculator tests', function () {
     beforeEach(() => {
         // open browser
