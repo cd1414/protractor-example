1ec645b1a21b3c46d2cf7e68471b32112913a7c9
Christian Quesada B <cd1414@gmail.com>
2020-09-12 10:09:38 -0600

    Clean conf.js

--- conf/conf.js
+++ conf/conf.js
@@ -48,35 +48,4 @@ exports.config = {
       new HTMLReport().from('xmlresults.xml', testConfig);
     });
   },
-
-  onPrepare: function () {
-    var jasmineReporters = require('jasmine-reporters');
-    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
-      consolidateAll: true,
-      savePath: './',
-      filePrefix: 'xmlresults'
-    }));
-
-    var fs = require('fs-extra');
-
-    fs.emptyDir('screenshots/', function (err) {
-      console.log(err);
-    });
-
-    jasmine.getEnv().addReporter({
-      specDone: function (result) {
-        if (result.status == 'failed') {
-          browser.getCapabilities().then(function (caps) {
-            var browserName = caps.get('browserName');
-
-            browser.takeScreenshot().then(function (png) {
-              var stream = fs.createWriteStream('screenshots/' + browserName + '-' + result.fullName + '.png');
-              stream.write(new Buffer(png, 'base64'));
-              stream.end();
-            });
-          });
-        }
-      }
-    });
-  }
 };
